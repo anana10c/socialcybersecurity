a4df2065d73fded41d076a47718a598c77de3ea0
Anna Cai <anana10c@gmail.com>
2020-06-28 22:44:29 -0400

    Updates

---
+++ precache-manifest.ca70b3fbbacf951a64eed82f88e9a175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102dd6edb901a09d8e8eb84e67475a9e",
+    "url": "/socialcybersecurity/index.html"
+  },
+  {
+    "revision": "cff1fc8a8ba12c109c62",
+    "url": "/socialcybersecurity/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0b17c63058251f40346",
+    "url": "/socialcybersecurity/static/js/2.7a75e679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/socialcybersecurity/static/js/2.7a75e679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff1fc8a8ba12c109c62",
+    "url": "/socialcybersecurity/static/js/main.0238aebb.chunk.js"
+  },
+  {
+    "revision": "a5f8de08d56de9127689",
+    "url": "/socialcybersecurity/static/js/runtime-main.c6ab0b42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socialcybersecurity/static/media/logo.5d5d9eef.svg"
+  }
+]);
