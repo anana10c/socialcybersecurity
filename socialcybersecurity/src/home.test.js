2077c55722ada7fa16c5cbdf2cb60fc5bd26a97b
Anna Cai <anana10c@gmail.com>
2020-06-30 15:59:00 -0400

    Delete home.test.js

--- socialcybersecurity/src/home.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from './home';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Home />);
-  const linkElement = getByText(/login/i);
-  expect(linkElement).toBeInTheDocument();
-});
